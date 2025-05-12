5a5c9c56c65733d035c86c8499e72a195c09e3b0
rodion <r0dion.pan@yandex.ru>
2025-05-12 18:14:35 +0300

    end

---
+++ yatube_api/api/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import get_object_or_404
+
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from api.permissions import IsAuthorOrReadOnly
+from api.serializers import (
+    CommentSerializer,
+    GroupSerializer,
+    PostSerializer,
+    UserSerializer,
+)
+from posts.models import Group, Post, User
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsAuthenticated, IsAuthorOrReadOnly)
+
+    def perform_create(self, serializer):
+        return serializer.save(author=self.request.user)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = (IsAuthenticated,)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (
+        IsAuthenticated,
+        IsAuthorOrReadOnly,
+    )
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, pk=self.kwargs.get("post_id"))
+        return post.comments.all()
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, pk=self.kwargs.get("post_id"))
+        serializer.save(author=self.request.user, post=post)
